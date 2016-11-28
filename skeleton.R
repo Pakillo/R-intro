b915e5405e6f694e89a761988460b822594a6637
Pakillo <f.rodriguez.sanc@gmail.com>
2016-11-28 16:27:38 +0100

    added intro skeleton

---
+++ skeleton.R
@@ -0,0 +1,37 @@
+## R and Rstudio overview
+
+## Rstudio projects
+
+## R as calculator
+
+## Assignment
+
+## Vectors
+
+## Data frames
+
+## NAs
+
+## Calling functions (sum, mean, log)
+
+## Getting help: ?, google, stackoverflow, Rdocumentation, Rseek
+
+## Reading in data
+
+## Indexing, subsetting, which
+
+## apply, tapply
+
+## Packages
+
+
+
+##
+
+## ggplot
+
+## tidyr & dplyr
+
+## modelr
+
+## rmarkdown
